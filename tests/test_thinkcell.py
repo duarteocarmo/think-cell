048e061e69cb656ae3496e69e125ea5fc353b113
Duarte OC <duarteocarmo@gmail.com>
2019-06-16 17:35:29 +0200

    finished tests 0.1

--- tests/test_thinkcell.py
+++ tests/test_thinkcell.py
@@ -1,5 +1,6 @@
 import pytest
 from thinkcell import Thinkcell
+from datetime import datetime
 
 
 class TestThinkcell(object):
@@ -10,6 +11,18 @@ class TestThinkcell(object):
     def test_str(self):
         tc = Thinkcell()
         assert hasattr(tc, "__str__") is True
+
+    @pytest.mark.parametrize(
+        "test_input, expected",
+        [
+            ("daf", {"string": "daf"}),
+            (3, {"number": 3}),
+            (2.0, {"number": 2.0}),
+            (datetime(2012, 9, 16, 0, 0), {"date": "2012-09-16"}),
+        ],
+    )
+    def test_transform_input(self, test_input, expected):
+        assert Thinkcell.transform_input(test_input) == expected
 
     def test_verify_template_1(self):
         template_name = "not a file name"
@@ -25,8 +38,91 @@ class TestThinkcell(object):
         template_name = "example.pptx"
         assert Thinkcell.verify_template(template_name) == template_name
 
-    def test_add_page(self):
+    def test_add_template(self):
         tc = Thinkcell()
         template = "example.pptx"
-        tc.add_page(template)
+        tc.add_template(template)
         assert tc.charts == [{"template": template, "data": []}]
+
+    def test_add_chart_bad_template(self):
+        tc = Thinkcell()
+        template = "example.pptx"
+        with pytest.raises(ValueError) as e_info:
+            tc.add_chart(
+                template_name="example2.pptx",
+                chart_name="Cool Name bro",
+                categories=["Alpha", "bravo"],
+                data=[[3, 4, datetime(2012, 9, 16, 0, 0)], [2, "adokf", 6]],
+            )
+
+    def test_add_chart_bad_dimensions(self):
+        tc = Thinkcell()
+        template = "example.pptx"
+        with pytest.raises(ValueError) as e_info:
+            tc.add_chart(
+                template_name="example2.pptx",
+                chart_name="Cool Name bro",
+                categories=["Alpha", "bravo"],
+                data=[[3, 4, datetime(2012, 9, 16, 0, 0)], [2, "adokf"]],
+            )
+
+    def test_add_chart(self):
+        tc = Thinkcell()
+        template = "example.pptx"
+        tc.add_template(template)
+        tc.add_chart(
+            template_name=template,
+            chart_name="Cool Name bro",
+            categories=["Alpha", "bravo"],
+            data=[[3, 4, datetime(2012, 9, 16, 0, 0)], [2, "adokf", 4]],
+        )
+        assert tc.charts == [
+            {
+                "template": "example.pptx",
+                "data": [
+                    {
+                        "name": "Cool Name bro",
+                        "table": [
+                            [None, {"string": "Alpha"}, {"string": "bravo"}],
+                            [],
+                            [
+                                {"number": 3},
+                                {"number": 4},
+                                {"date": "2012-09-16"},
+                            ],
+                            [
+                                {"number": 2},
+                                {"string": "adokf"},
+                                {"number": 4},
+                            ],
+                        ],
+                    }
+                ],
+            }
+        ]
+
+    @pytest.mark.parametrize(
+        "input, output", [("word.docx", ValueError), (3, ValueError)]
+    )
+    def test_save_ppttc_bad_file(self, input, output):
+        tc = Thinkcell()
+        tc.add_template("example.pptx")
+        tc.add_chart(
+            template_name="example.pptx",
+            chart_name="Chart name",
+            categories=["alpha", "bravo"],
+            data=[["today", 1, 2], ["tomorrow", 3, 4]],
+        )
+        with pytest.raises(output) as e_info:
+            tc.save_ppttc(filename=input)
+
+    def test_save_ppttc(self):
+        tc = Thinkcell()
+        tc.add_template("example.pptx")
+        tc.add_chart(
+            template_name="example.pptx",
+            chart_name="Chart name",
+            categories=["alpha", "bravo"],
+            data=[["today", 1, 2], ["tomorrow", 3, 4]],
+        )
+        assert tc.save_ppttc(filename="test.ppttc") == True
